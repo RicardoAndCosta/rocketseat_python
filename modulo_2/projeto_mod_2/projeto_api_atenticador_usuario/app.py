78136fc37569b6040acc80838f91f4883993530c
RicardoAndCosta <jricardoandradecosta@hotmail.com>
2024-06-06 09:15:03 -0300

    Ajustando a estrutura de arquivos

--- modulo_2/projeto_mod_2/projeto_api_atenticador_usuario/app.py
+++ modulo_2/projeto_mod_2/projeto_api_atenticador_usuario/app.py
@@ -29,13 +29,14 @@ def login():
     password = data.get("password")
 
     if username and password:
-        print ({"username": username, "password": password}) # Se retirar o # e executar o comando é retornado as informações do usuário
+        #print ({"username": username, "password": password}) # Se retirar o # e executar o comando é retornado as informações do usuário
         user = User.query.filter_by(username=username).first()
         
         if user and user.password == password:
             login_user(user)
             print(current_user.is_authenticated)
             return jsonify({"message": "Acesso autorizado!"})
+        
     return jsonify({"message": "Credenciais inválidas"}), 400
 
 @app.route('/logout', methods=['GET'])
@@ -43,6 +44,22 @@ def login():
 def logout():
    logout_user()
    return jsonify({"message": "Logout realizado com sucesso!"}) 
+
+@app.route('/user', methods=['POST'])
+def create_user():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+      user = User(username=username, password=password)
+      db.session.add(user)
+      db.session.commit()
+
+      return jsonify({"message": "Usuário cadastrado com sucesso!"})
+  
+  return jsonify({"message": "Dados invalidos"}), 400
+
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
